0093fd6711dd67df06be0dd4acdd46edbe0d70c2
Jason Charnes <jason@thecharnes.com>
2019-04-29 21:41:14 -0500

    Adds spacing; Updates dummy app

--- test/dummy/app/madmin/resources/user_resource.rb
+++ test/dummy/app/madmin/resources/user_resource.rb
@@ -3,10 +3,10 @@ module Madmin
     class UserResource
       include Madmin::Resourceable
 
-      field :id, Madmin::Field::Number, table: true
-      field :email, Madmin::Field::Email, table: true, write: true, label: "Customer email"
-      field :first_name, Madmin::Field::Text, table: true, write: true
-      field :last_name, Madmin::Field::Text, table: true, write: true
+      field :id, Madmin::Field::Number, index: true
+      field :email, Madmin::Field::Email, index: true, form: true, label: "Customer email"
+      field :first_name, Madmin::Field::Text, index: true, form: true
+      field :last_name, Madmin::Field::Text, index: true, form: true
     end
   end
 end
